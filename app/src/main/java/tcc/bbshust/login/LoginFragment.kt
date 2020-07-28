a3730ea4073cf04a40aaa9aca69debd2dfd84d4d
Banson Xu <xzcxzcyy@126.com>
2020-07-28 16:54:59 +0800

    feat: login fragment ui

--- app/src/main/java/tcc/bbshust/login/LoginFragment.kt
+++ app/src/main/java/tcc/bbshust/login/LoginFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import tcc.bbshust.R
+import tcc.bbshust.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
@@ -15,18 +18,19 @@ class LoginFragment : Fragment() {
     }
 
     private lateinit var viewModel: LoginViewModel
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
     }
 
 }
