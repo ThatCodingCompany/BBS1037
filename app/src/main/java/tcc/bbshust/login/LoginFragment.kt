1a10a717345740a907fa6011ea898b989be8e681
Banson Xu <xzcxzcyy@126.com>
2020-07-28 15:51:01 +0800

    feat: login package

---
+++ app/src/main/java/tcc/bbshust/login/LoginFragment.kt
@@ -0,0 +1,32 @@
+package tcc.bbshust.login
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import tcc.bbshust.R
+
+class LoginFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LoginFragment()
+    }
+
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
