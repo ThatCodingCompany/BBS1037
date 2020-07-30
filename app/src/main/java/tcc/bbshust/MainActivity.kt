253124c4aa4849b6a28027c54f8e2f7bcc0de541
Chengyuan <1136427613@qq.com>
2020-07-30 13:26:45 +0800

    change the way of bottom navigation

--- app/src/main/java/tcc/bbshust/MainActivity.kt
+++ app/src/main/java/tcc/bbshust/MainActivity.kt
@@ -3,6 +3,8 @@ package tcc.bbshust
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import tcc.bbshust.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navView=binding.navView
+        val navController=findNavController(R.id.navHostFragment)
+
+        navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/tcc/bbshust/login/LoginFragment.kt
+++ app/src/main/java/tcc/bbshust/login/LoginFragment.kt
@@ -2,12 +2,14 @@ package tcc.bbshust.login
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import tcc.bbshust.R
 import tcc.bbshust.databinding.FragmentLoginBinding
 
@@ -19,7 +21,7 @@ class LoginFragment : Fragment() {
 
     private lateinit var viewModel: LoginViewModel
     private lateinit var binding: FragmentLoginBinding
-
+    val TAG = "LoginFragment :"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,6 +31,13 @@ class LoginFragment : Fragment() {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
         binding.lifecycleOwner = this
+
+        binding.buttonLogin.setOnClickListener {
+            Log.d(TAG, "onCreateView: before nav")
+            this.findNavController()
+                .navigate(LoginFragmentDirections.actionLoginFragmentToHomeFragment())
+            Log.d(TAG, "onCreateView: after nav")
+        }
 
         binding.viewModel = viewModel
 
