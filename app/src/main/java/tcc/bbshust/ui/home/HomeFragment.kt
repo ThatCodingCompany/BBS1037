d1caba80f55e3bf4c07a04a4b9231c3dc0d429ed
Banson Xu <xzcxzcyy@126.com>
2020-07-31 19:42:01 +0800

    feat: ui for home

--- app/src/main/java/tcc/bbshust/ui/home/HomeFragment.kt
+++ app/src/main/java/tcc/bbshust/ui/home/HomeFragment.kt
@@ -1,35 +1,67 @@
 package tcc.bbshust.ui.home
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import tcc.bbshust.R
 import tcc.bbshust.databinding.HomeFragmentBinding
+import tcc.bbshust.network.data.Post
+
+private const val TAG = "HomeFragment"
 
 class HomeFragment : Fragment() {
 
     private lateinit var viewModel: HomeViewModel
-    private lateinit var binding:HomeFragmentBinding
+    private lateinit var binding: HomeFragmentBinding
+    private lateinit var postAdapter: PostAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        viewModel=ViewModelProvider(this).get(HomeViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         binding = DataBindingUtil.inflate(inflater, R.layout.home_fragment, container, false)
 
-        binding.lifecycleOwner=this
-        binding.viewModel=viewModel
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        var i = 1
+        val l = mutableListOf<Post>()
+        while (i <= 30) {
+            val tp = Post(
+                i.toString(),
+                System.currentTimeMillis(),
+                System.currentTimeMillis(),
+                "Title$i",
+                "Author$i",
+                "Content$i",
+                listOf<Post>()
+            )
+            l.add(tp)
+            i += 1
+        }
+
+        postAdapter = PostAdapter(PostListener {
+            Log.d(TAG, "onCreateView: item in RECYCLERVIEW clicked.")
+        })
+
+        binding.postRecyclerview.adapter = postAdapter
+
+        /*viewModel.postList.observe(viewLifecycleOwner, Observer { list ->
+            postAdapter.submitList(list)
+        })*/
+
+        postAdapter.submitList(l)
 
         return binding.root
     }
-
 
 
 }
--- app/src/main/java/tcc/bbshust/ui/home/HomeViewModel.kt
+++ app/src/main/java/tcc/bbshust/ui/home/HomeViewModel.kt
@@ -3,13 +3,22 @@ package tcc.bbshust.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import tcc.bbshust.network.NetworkApi
+import tcc.bbshust.network.data.Post
 
 class HomeViewModel : ViewModel() {
+    private val viewModelJob = Job()
+    private val uiScope = CoroutineScope(viewModelJob + Dispatchers.Main)
 
-    private val _hint=MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    val postList = MutableLiveData<MutableList<Post>>()
+
+    init {
+//        uiScope.launch {
+//            val res = NetworkApi.retrofitService.getAllPostsAsync()
+//        }
     }
-
-    val hint:LiveData<String>
-        get() = _hint
 }
